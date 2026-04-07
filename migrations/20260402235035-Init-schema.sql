15d26c73d8729997ad13157c74ee3263d1005c98
Konrad G <konradg.net@gmail.com>
2026-04-07 02:13:51 +0200

    Made migration more readable

--- migrations/20260402235035-Init-schema.sql
+++ migrations/20260402235035-Init-schema.sql
@@ -1,17 +1,97 @@
 -- UP
-CREATE TABLE IF NOT EXISTS `logs` (`id` CHAR(36) NOT NULL DEFAULT (UUID()), `title` TEXT, `type` VARCHAR(255), `description` TEXT, `createdAt` DATETIME NOT NULL, `updatedAt` DATETIME NOT NULL, PRIMARY KEY (`id`));
+CREATE TABLE IF NOT EXISTS `logs` (
+  `id` CHAR(36) NOT NULL DEFAULT (UUID()),
+  `title` TEXT,
+  `type` VARCHAR(255),
+  `description` TEXT,
+  `createdAt` DATETIME NOT NULL,
+  `updatedAt` DATETIME NOT NULL,
+  PRIMARY KEY (`id`)
+);
 
-CREATE TABLE IF NOT EXISTS `files` (`id` CHAR(36) NOT NULL DEFAULT (UUID()), `key` VARCHAR(255) UNIQUE, `isPublic` TINYINT(1) DEFAULT 0, `data` LONGBLOB, `s3Key` VARCHAR(255), `name` VARCHAR(255), `storageType` VARCHAR(255), `encoding` VARCHAR(255), `mimetype` VARCHAR(255), `size` BIGINT, `md5` VARCHAR(255), `createdAt` DATETIME NOT NULL, `updatedAt` DATETIME NOT NULL, PRIMARY KEY (`id`));
+CREATE TABLE IF NOT EXISTS `files` (
+  `id` CHAR(36) NOT NULL DEFAULT (UUID()),
+  `key` VARCHAR(255) UNIQUE,
+  `isPublic` TINYINT(1) DEFAULT 0,
+  `data` LONGBLOB,
+  `s3Key` VARCHAR(255),
+  `name` VARCHAR(255),
+  `storageType` VARCHAR(255),
+  `encoding` VARCHAR(255),
+  `mimetype` VARCHAR(255),
+  `size` BIGINT,
+  `md5` VARCHAR(255),
+  `createdAt` DATETIME NOT NULL,
+  `updatedAt` DATETIME NOT NULL,
+  PRIMARY KEY (`id`)
+);
 
-CREATE TABLE IF NOT EXISTS `image_files` (`id` CHAR(36) NOT NULL DEFAULT (UUID()), `key` VARCHAR(255) UNIQUE, `nameDisplayed` VARCHAR(255), `fileId` CHAR(36), `isPublic` TINYINT(1) DEFAULT 0, `createdAt` DATETIME NOT NULL, `updatedAt` DATETIME NOT NULL, PRIMARY KEY (`id`), FOREIGN KEY (`fileId`) REFERENCES `files` (`id`) ON DELETE SET NULL ON UPDATE CASCADE);
+CREATE TABLE IF NOT EXISTS `image_files` (
+  `id` CHAR(36) NOT NULL DEFAULT (UUID()),
+  `key` VARCHAR(255) UNIQUE,
+  `nameDisplayed` VARCHAR(255),
+  `fileId` CHAR(36),
+  `isPublic` TINYINT(1) DEFAULT 0,
+  `createdAt` DATETIME NOT NULL,
+  `updatedAt` DATETIME NOT NULL,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`fileId`) REFERENCES `files` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
+);
 
-CREATE TABLE IF NOT EXISTS `systems` (`id` CHAR(36) NOT NULL DEFAULT (UUID()), `isSetupFinished` TINYINT(1), `createdAt` DATETIME NOT NULL, `updatedAt` DATETIME NOT NULL, PRIMARY KEY (`id`));
+CREATE TABLE IF NOT EXISTS `systems` (
+  `id` CHAR(36) NOT NULL DEFAULT (UUID()),
+  `isSetupFinished` TINYINT(1),
+  `createdAt` DATETIME NOT NULL,
+  `updatedAt` DATETIME NOT NULL,
+  PRIMARY KEY (`id`)
+);
 
-CREATE TABLE IF NOT EXISTS `users` (`id` CHAR(36) NOT NULL DEFAULT (UUID()), `email` VARCHAR(255) UNIQUE, `emailVerified` TINYINT(1) DEFAULT 0, `emailVerificationCode` VARCHAR(255), `verificationCodeEmailSentDate` DATETIME, `password` VARCHAR(255), `failedLoginAttempts` INTEGER DEFAULT 0, `lastFailedLoginAttempt` DATETIME, `loginProviders` JSON DEFAULT ('[]'), `firstName` VARCHAR(255), `lastName` VARCHAR(255), `photoId` CHAR(36), `status` VARCHAR(255) DEFAULT 'Live', `notes` TEXT, `role` VARCHAR(255) DEFAULT 'User', `createdAt` DATETIME NOT NULL, `updatedAt` DATETIME NOT NULL, PRIMARY KEY (`id`), FOREIGN KEY (`photoId`) REFERENCES `image_files` (`id`) ON DELETE SET NULL ON UPDATE CASCADE);
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` CHAR(36) NOT NULL DEFAULT (UUID()),
+  `email` VARCHAR(255) UNIQUE,
+  `emailVerified` TINYINT(1) DEFAULT 0,
+  `emailVerificationCode` VARCHAR(255),
+  `verificationCodeEmailSentDate` DATETIME,
+  `password` VARCHAR(255),
+  `failedLoginAttempts` INTEGER DEFAULT 0,
+  `lastFailedLoginAttempt` DATETIME,
+  `loginProviders` JSON DEFAULT ('[]'),
+  `firstName` VARCHAR(255),
+  `lastName` VARCHAR(255),
+  `photoId` CHAR(36),
+  `status` VARCHAR(255) DEFAULT 'Live',
+  `notes` TEXT,
+  `role` VARCHAR(255) DEFAULT 'User',
+  `createdAt` DATETIME NOT NULL,
+  `updatedAt` DATETIME NOT NULL,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`photoId`) REFERENCES `image_files` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
+);
 
-CREATE TABLE IF NOT EXISTS `user_sessions` (`id` CHAR(36) NOT NULL DEFAULT (UUID()), `userId` CHAR(36), `sessionId` VARCHAR(255) UNIQUE, `ip` VARCHAR(255), `userAgent` VARCHAR(255), `expiresDate` DATETIME, `createdAt` DATETIME NOT NULL, `updatedAt` DATETIME NOT NULL, PRIMARY KEY (`id`), FOREIGN KEY (`userId`) REFERENCES `users` (`id`) ON DELETE SET NULL ON UPDATE CASCADE);
+CREATE TABLE IF NOT EXISTS `user_sessions` (
+  `id` CHAR(36) NOT NULL DEFAULT (UUID()),
+  `userId` CHAR(36),
+  `sessionId` VARCHAR(255) UNIQUE,
+  `ip` VARCHAR(255),
+  `userAgent` VARCHAR(255),
+  `expiresDate` DATETIME,
+  `createdAt` DATETIME NOT NULL,
+  `updatedAt` DATETIME NOT NULL,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`userId`) REFERENCES `users` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
+);
 
-CREATE TABLE IF NOT EXISTS `posts` (`id` CHAR(36) NOT NULL DEFAULT (UUID()), `key` VARCHAR(255) UNIQUE, `title` TEXT, `description` TEXT, `content` TEXT, `publishedDate` DATETIME, `createdAt` DATETIME NOT NULL, `updatedAt` DATETIME NOT NULL, PRIMARY KEY (`id`));
+CREATE TABLE IF NOT EXISTS `posts` (
+  `id` CHAR(36) NOT NULL DEFAULT (UUID()),
+  `key` VARCHAR(255) UNIQUE,
+  `title` TEXT,
+  `description` TEXT,
+  `content` TEXT,
+  `publishedDate` DATETIME,
+  `createdAt` DATETIME NOT NULL,
+  `updatedAt` DATETIME NOT NULL,
+  PRIMARY KEY (`id`)
+);
 
 -- DOWN
 DROP TABLE IF EXISTS `posts`;
